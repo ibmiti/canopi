e67a8eaa7be07813450fad581bc6efe4a7ddc05e
Bmiti <Bmiti@Bmitis-MacBook-Air.local>
2019-12-21 09:30:43 -0500

    making changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div id="banner-wrapper">
       <div id="banner" class="container">
         <h2>Under the Canopi</h2>
-        <p>This is <strong>7 Steps</strong> we blog we doo dah and doo dan</p>
+        <p>This is <strong>7 Steps</strong> we blog </p>
         <!-- <a href="#" class="button">Her Lung</a>
         <a href="#" class="button">Canvas</a>
         <a href="#" class="button">7 steps</a> -->
